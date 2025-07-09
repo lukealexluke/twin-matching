a253078c48277048959777b37323310ecf35b54a
lukealexluke <lukealexander280@gmail.com>
2025-07-09 03:41:32 -0700

    matching and ordering mechanism

--- twin.py
+++ twin.py
@@ -1,18 +1,33 @@
 import statistics
-import pandas as pd
+from math import log
+import polars as pl
+import numpy as np
 
-def matching_procedure(control: pd.DataFrame, treat: pd.DataFrame, singleton: bool, unique: bool):
+def matching_procedure(treat: pl.DataFrame, control: pl.DataFrame, count: int):
     """
     Matches elements of control group to elements of treatment group
     based on the path of the outcome variable being measured.
     Args:
        control: DataFrame of control group and time-series data
        treat: DataFrame of treatment group and time-series data
-       singleton: whether treatments should only match to one variable
-       unique: whether treatments should match uniquely
+       count: number of matches per treatment variable
 
     Returns:
         matches: dict object of treatment group and control matches ranked by correlation,
         contains only one match if singleton=True
     """
-    pass
+    if control.shape[1] > count:
+        raise ValueError("Number of matches requested exceeds elements in control group")
+    t_cols = treat.columns
+    df = treat + control
+
+    # evaluate Y_i,t and set up new data frame (D.3a-c), (D.4a,c)
+
+    matches = {}
+    for i, _ in enumerate(t_cols):
+        rest = t_cols[:i] + t_cols[i+1:]
+        holder = df.drop(rest)
+        columns_sorted = sorted(holder.columns, key=lambda col: df[col][i], reverse=True)
+        matches[t_cols[i]] = columns_sorted[1:count+1]
+
+    return matches
